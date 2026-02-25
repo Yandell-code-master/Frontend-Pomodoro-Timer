930888d8079d842778263c1f69b2f6c807fa950a
Yandell-code-master <152900887+Yandell-code-master@users.noreply.github.com>
2026-02-25 14:32:46 -0600

    agregué el modal de las settings

--- js/env.js
+++ js/env.js
@@ -2,7 +2,7 @@ window.ENV = {
     API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:8080/'
         : 'https://backend-pomodoro-timer-production.up.railway.app/',
-    URL_FRONTEND : window.localStorage.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-        ? "http://localhost:800"
+    URL_FRONTEND : window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+        ? "http://localhost:8000"
         : "http://yandell-code-master/Pomodoro-Timer"
 };
